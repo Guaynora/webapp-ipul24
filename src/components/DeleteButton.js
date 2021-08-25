761aed38302e467549bef65e512a332acfa97086
Guaynora <guaynora13@gmail.com>
2021-08-25 18:49:34 -0500

    Add button Delete and Edit

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DeleteButton() {
+  return (
+    <span className="icon has-text-danger">
+      <i className="far fa-lg fa-trash-alt"></i>
+    </span>
+  );
+}
+
+export default DeleteButton;
---
+++ src/components/EditButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EditButton() {
+  return (
+    <span className="icon has-text-success mr-2">
+      <i className="far fa-lg fa-edit"></i>
+    </span>
+  );
+}
+
+export default EditButton;
