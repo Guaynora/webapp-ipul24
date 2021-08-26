98a0ef80702eec0901f20fe077f30a781f8f4d40
Guaynora <guaynora13@gmail.com>
2021-08-26 00:14:56 -0500

    Remove button send in form and add button in pages Add

--- src/pages/AddMember.js
+++ src/pages/AddMember.js
@@ -1,12 +1,16 @@
 import React from "react";
 import FormMember from "../components/FormMember";
+import SubmitButton from "../components/SubmitButton";
 import TopComponent from "../components/TopComponent";
 
 function AddMember() {
   return (
     <section className="container">
       <TopComponent back={true} />
-      <h3 className="title is-4 mx-6 mt-4">Agregar miembro</h3>
+      <div className="title-button mx-6 mt-4">
+        <h3 className="title is-4 m-0">Agregar miembro</h3>
+        <SubmitButton />
+      </div>
       <div className="form-container p-6">
         <FormMember />
       </div>
