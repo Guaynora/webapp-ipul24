545032ac00db9f0086c417122ee2d146ab8db33d
Guaynora <guaynora13@gmail.com>
2021-09-01 18:47:31 -0500

    add button for Delete and Edit

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-function DeleteButton() {
+function DeleteButton({ type }) {
+  const urls = {
+    member: "http://localhost:1337/members",
+    tithe: "http://localhost:1337/tithes",
+  };
+
+  const Delete = (e) => {};
+
   return (
-    <span className="icon has-text-danger">
+    <span className="icon has-text-danger" onClick={Delete}>
       <i className="far fa-lg fa-trash-alt"></i>
     </span>
   );
--- src/components/EditButton.js
+++ src/components/EditButton.js
@@ -1,8 +1,17 @@
-import React from "react";
+import { useHistory } from "react-router-dom";
 
-function EditButton() {
+function EditButton({ type, data }) {
+  let history = useHistory();
+  const urls = {
+    member: `/member/${data.id}`,
+    tithe: `/tithe/${data.id}`,
+  };
+
+  const Update = (e) => {
+    history.push(urls[type]);
+  };
   return (
-    <span className="icon has-text-success mr-2">
+    <span className="icon has-text-success mr-2" onClick={Update}>
       <i className="far fa-lg fa-edit"></i>
     </span>
   );
