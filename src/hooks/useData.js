acf70f71ee2f641c8edd7b4448bba3b08eb0c920
Guaynora <guaynora13@gmail.com>
2022-05-26 00:17:19 -0500

    refactoring to get members

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -6,29 +6,33 @@ function useData(url) {
   const { token } = useContext(AuthContext);
 
   useEffect(() => {
-    const controller = new AbortController();
-    const getData = async (url) => {
-      try {
-        let res = await fetch(url, {
-          headers: { Authorization: `Bearer ${token}` },
-          signal: controller.signal,
-        });
-
-        if (!res.ok) {
-          throw new Error({
-            err: true,
-            status: res.status,
-            statusText: !res.status.Text ? "ocurrio un error" : res.statusText,
+    if (data.length === 0) {
+      const controller = new AbortController();
+      const getData = async (url) => {
+        try {
+          let res = await fetch(url, {
+            headers: { Authorization: `Bearer ${token}` },
+            signal: controller.signal,
           });
+
+          if (!res.ok) {
+            throw new Error({
+              err: true,
+              status: res.status,
+              statusText: !res.status.Text
+                ? "ocurrio un error"
+                : res.statusText,
+            });
+          }
+          let data = await res.json();
+          setData(data);
+        } catch (error) {
+          console.log(error);
         }
-        let data = await res.json();
-        setData(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getData(url);
-    return () => controller.abort();
+      };
+      getData(url);
+      return () => controller.abort();
+    }
   }, [data]);
 
   return { data };
