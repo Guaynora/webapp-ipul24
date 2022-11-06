8b7e51b75ed3f73af4d9205eb636c8ca9f4e65c8
= <guaynora13@gmail.com>
2022-11-05 20:05:25 -0500

    show percents in tithes

--- src/pages/Tithe/index.js
+++ src/pages/Tithe/index.js
@@ -10,6 +10,9 @@ function Tithe() {
   const { token } = useContext(AuthContext);
 
   const { data } = useData(`${process.env.REACT_APP_APIURL}/tithes`);
+  const totalTithes = useData(
+    `${process.env.REACT_APP_APIURL}/tithes/percents`
+  );
 
   if (!token) {
     return <Redirect to="/login" />;
@@ -21,6 +24,24 @@ function Tithe() {
       <div className="title-button mx-6 mt-4">
         <h3 className="title is-4 m-0">Diezmos</h3>
         <AddButton text="diezmo" />
+      </div>
+      <div className="container-values mx-6 mt-4">
+        <div className="cardshow-values">
+          <p className="title-cardshow">Total Diezmo</p>
+          <p>{totalTithes.data.total}</p>
+        </div>
+        <div className="cardshow-values">
+          <p className="title-cardshow">10%</p>
+          <p>{totalTithes.data.tenPercent}</p>
+        </div>
+        <div className="cardshow-values">
+          <p className="title-cardshow">2%</p>
+          <p>{totalTithes.data.twoPercent}</p>
+        </div>
+        <div className="cardshow-values">
+          <p className="title-cardshow">5%</p>
+          <p>{totalTithes.data.fivePercent}</p>
+        </div>
       </div>
       <div className="table-container p-6">
         <Table datas={data} type="tithe" />
